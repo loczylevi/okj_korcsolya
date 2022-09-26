536dfeb9aa3b619031631c867b76e1c2a8f2f5c9
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2022-09-26 12:29:36 +0200

    Update korcsolya-oop.py

--- korcsolya-oop.py
+++ korcsolya-oop.py
@@ -65,3 +65,46 @@ for sor in donto:
 
 print("7. feladat")
 statisztika = [print(f"        {orszag}: {db} versenyző") for orszag,db in stat.items() if db > 1]
+    
+    
+def Pontok_alapjan():
+    kereso = [sor for sor in lista if sor.nev == bekeres]
+    if kereso:
+        pontszamok = [(sor.technika,sor.komponens,sor.hiba) for sor in kereso]
+        return (pontszamok[0][0] + pontszamok[0][1]) - pontszamok[0][2]
+    else:
+        return "        Ilyen nevű induló nem volt"
+    
+with open("vegeredmeny.csv","w",encoding="UTF-8") as f3:
+    for sor in donto:
+            jatekos = sor.nev_d
+            pontszamok_d = [(sor.technika_d,sor.komponens_d,sor.hiba_d) for sor in donto if sor.nev_d == jatekos]
+            jatekos_adatai = [sor for sor in donto if sor.nev_d == jatekos]
+            pontszam_x_jatekosnak = (pontszamok_d[0][0] + pontszamok_d[0][1]) - pontszamok_d[0][2]
+            pontszamok2 = [(sor.technika,sor.komponens,sor.hiba) for sor in lista if sor.nev == jatekos]
+            pontszam_x_jatekosnak2 = (pontszamok2[0][0] + pontszamok2[0][1]) - pontszamok2[0][2]
+            ossz11 = float(pontszam_x_jatekosnak2) + float(pontszam_x_jatekosnak)
+            ossz11 = f"{ossz11:.2f}"
+            f3.write(f"{jatekos};{jatekos_adatai[0].orszag_d};{ossz11}\n")
+
+class Vegeredmeny:
+    def __init__(self,sor):
+        nev,orszag,pont = sor.strip().split(";")
+        self.nev = nev
+        self.orszag = orszag
+        self.pont = float(pont)
+
+with open("vegeredmeny.csv","r",encoding="UTF-8") as f2:
+    lista = [Vegeredmeny(sor) for sor in f2]
+    
+with open("vegeredmeny.csv","w",encoding="UTF-8") as f4:
+    lista.sort(key=lambda x:x.pont,reverse=True)
+    for sor in lista:
+        f4.write(f"{sor.nev}:{sor.orszag} {str(sor.pont)}\n")
+        
+        
+with open("vegeredmeny.csv","w",encoding="UTF-8") as f5:
+    szamlalo = 1
+    for sor in lista:
+        f5.write(f"{szamlalo};{sor.nev}:{sor.orszag} {str(sor.pont)}\n")
+        szamlalo = szamlalo + 1
